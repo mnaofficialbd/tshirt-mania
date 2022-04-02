1f96892dfba322226f08d6d90121d1052aaf00a0
mnaofficial <mna.official92@gmail.com>
2022-04-02 23:04:47 +0600

    item handle

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Cart.css'
 
-const Cart = () => {
+const Cart = ({ cart, handleRemoveFromCart }) => {
     return (
         <div>
-            <h3>This is Cart</h3>
+            <h2>Items Selected: {cart.length}</h2>
+            {
+                cart.map(tShirt => <p>
+                    {tShirt.name}
+                    <button onClick={() => handleRemoveFromCart(tShirt)}>X</button>
+                </p>)
+            }
         </div>
     );
 };
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,12 +6,24 @@ import "./Home.css"
 
 const Home = () => {
     const [tShirts, setTShirts] = useTShirts();
-    const [cart,setCart]=useState([])
+    const [cart, setCart] = useState([])
 
-    const handleAddToCart=(selectedItem)=>{
-const newCart=[...cart,selectedItem];
-setCart(newCart)
+    const handleAddToCart = (selectedItem) => {
+        const exists=cart.find(tShirt=>tShirt._id ===selectedItem._id);
+        if(!exists){
+            const newCart = [...cart, selectedItem];
+            setCart(newCart)
+        }
+        else{
+            alert('Item already added')
+        }
     }
+
+    const handleRemoveFromCart = (selectedItem) => {
+        const rest = cart.filter(tShirt => tShirt._id !== selectedItem._id);
+        setCart(rest);
+    }
+
     return (
         <div className='home-container'>
             <div className="tShirt-container">
@@ -24,7 +36,10 @@ setCart(newCart)
                 }
             </div>
             <div className="cart-container">
-                <Cart></Cart>
+                <Cart
+                    handleRemoveFromCart={handleRemoveFromCart}
+                    cart={cart}
+                ></Cart>
             </div>
         </div>
     );
